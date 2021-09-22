89f727468ae0aa79a1b94b9c3b5a0992af2df6cf
Leo-Kopec <lfkopec@loyola,edu>
2021-09-22 11:43:02 -0400

    commit 01

---
+++ Labs/Lab#1.py
@@ -0,0 +1,19 @@
+#Programmers: Leo Kopec
+#Course: CS151, Prof. Mehri
+#Date: September 23, 2021
+#Lab Number: #1
+#Program Inputs: volume_mL
+#Program Outputs: table_spoon, tea_spoon
+
+
+
+#Start
+#volume_mL:float
+#tea_spoon:int
+#table_spoon:int
+#tea_spoon = volume_mL // 5
+#table_spoon = tea_spoon // 3
+#adjust tea_spoon
+#input: "Enter a volime in mL:", volume_mL
+#output: "table spoons =", table_spoon, "tea spoons =", tea_spoons
+#End
